b421d7561e41871f471c6dc2667a63bd970756cf
Thang <qthang1310@gmail.com>
2022-12-03 19:00:54 +0700

    update

--- client/src/utils/fetchData.js
+++ client/src/utils/fetchData.js
@@ -1,35 +1,37 @@
-import axios from 'axios';
+import axios from "axios";
+
+const baseUrl = "http://localhost:5000";
 
 export const getDataAPI = async (url, token) => {
-  const res = await axios.get(`/api/${url}`, {
+  const res = await axios.get(baseUrl + `/api/${url}`, {
     headers: { Authorization: token },
   });
   return res;
 };
 
 export const postDataAPI = async (url, post, token) => {
-  const res = await axios.post(`/api/${url}`, post, {
+  const res = await axios.post(baseUrl + `/api/${url}`, post, {
     headers: { Authorization: token },
   });
   return res;
 };
 
 export const putDataAPI = async (url, post, token) => {
-  const res = await axios.put(`/api/${url}`, post, {
+  const res = await axios.put(baseUrl + `/api/${url}`, post, {
     headers: { Authorization: token },
   });
   return res;
 };
 
 export const patchDataAPI = async (url, post, token) => {
-  const res = await axios.patch(`/api/${url}`, post, {
+  const res = await axios.patch(baseUrl + `/api/${url}`, post, {
     headers: { Authorization: token },
   });
   return res;
 };
 
 export const deleteDataAPI = async (url, token) => {
-  const res = await axios.delete(`/api/${url}`, {
+  const res = await axios.delete(baseUrl + `/api/${url}`, {
     headers: { Authorization: token },
   });
   return res;
