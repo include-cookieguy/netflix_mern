12da665e08f4cf64ebbc221cc8f755698446211c
Thang <thangnq111@ghtk.co>
2023-08-02 21:59:57 +0700

    refactor

--- client/src/redux/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import auth from "./authReducer";
-import alert from "./alertReducer";
-import genre from "./genreReducer";
-import user from "./userReducer";
-import search from "./searchReducer";
-
-export default combineReducers({ auth, alert, genre, user, search });
